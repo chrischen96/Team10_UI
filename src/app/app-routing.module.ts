2a53991e238af6d40211cd5f8d569545d5d695a4
chrischen96 <73258953+chrischen96@users.noreply.github.com>
2021-02-23 05:45:51 +0000

    Enter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'nav',
+    loadChildren: () => import('./nav/nav.module').then( m => m.NavPageModule)
+  },
 ];
 
 @NgModule({
